689dd676e87dc2c5aa987d82f5d8dfe236314807
Manjoura98 <89964355+Manjoura98@users.noreply.github.com>
2022-02-07 15:52:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -29,11 +29,11 @@ with open("/home/mehdim/Desktop/BioPython/chry5_S14_L001_R1_001.fastq") as file:
             #PourLe S commençant de la position 0 et avec un hope de longeur k, jusquau dernier kmer, Split to Kmers of HopeLength,PisIncrémenter s
             for s in range(0, len(line)-hope):
                 sub = line[s:s+hope]
-            if sub in newdict:
-                newdict[sub] += 1
+                if sub in newdict:
+                    newdict[sub] += 1
         #Sinon ajouter la première Ocurrence
-            else:
-                newdict[sub] = 1
+                else:
+                    newdict[sub] = 1
 
         #Passer à la prochaine ligne de séquence
             nLine = nLine + 4
