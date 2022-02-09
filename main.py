ecb4d84e30fc215ba3982d51c437bc563926d21c
Manjoura98 <89964355+Manjoura98@users.noreply.github.com>
2022-02-08 22:16:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,53 @@
-from pympler import tracker
 import time
-
-mem = tracker.SummaryTracker()
-#ChronoStart
-start = time.time()
-
-#OuvertureDuFichier
-with open("/home/mehdim/Desktop/BioPython/chry5_S14_L001_R1_001.fastq") as file:
-    #DéclarationDuDictio
-    newdict = {}
-    #InitialisationDeLaPremièreLigneAtraiter
-    nLine = 1
-    #LaLongeurDuKmer
-    hope = input("what is the size of the k desired:")
-    hope = int(hope)
-    #IterationSurLesLignesDuFichierPourTraiterLesLignesDesSéquences
-    for i, line in enumerate(file):
-        #ÇavaCommencerÀLaLigneAvecIndex=1 ---> nLine
-        if i == nLine:
-            #CalculDuNombreDesKmers
-            numbKmers = len(line) - hope + 1
-            print("La longueur de la séquence est de:", len(line), "Nucléotides")
-            print("La séquence est la suivante:", line)
-            print("Le nombre des kmers:", numbKmers)
+from pympler import tracker
 
 
-            #PourLe S commençant de la position 0 et avec un hope de longeur k, jusquau dernier kmer, Split to Kmers of HopeLength,PisIncrémenter s
-            for s in range(0, len(line)-hope):
-                sub = line[s:s+hope]
-                if sub in newdict:
-                    newdict[sub] += 1
-        #Sinon ajouter la première Ocurrence
-                else:
-                    newdict[sub] = 1
+def processingfunc (fname, hope):
+    mem = tracker.SummaryTracker()
+    # ChronoStart
+    start = time.time()
+    # OuvertureDuFichier
+    with open(fname, "r") as file:
+        # DéclarationDuDictio
+        newdict = {}
+        # InitialisationDeLaPremièreLigneAtraiter
+        nLine = 1
+        # LaLongeurDuKmer
 
-        #Passer à la prochaine ligne de séquence
-            nLine = nLine + 4
-        
-        #ChronoEnd
-    end = time.time()
-    #Calcul de la durée du Processus
-    processTiming = end - start
-    #Le dictionnaire avec le nombre d'occurences
-    print(newdict)
+        hope = int(hope)
+        # IterationSurLesLignesDuFichierPourTraiterLesLignesDesSéquences
+        for i, line in enumerate(file):
+            pline = line.replace('N', '')
+            # ÇavaCommencerÀLaLigneAvecIndex=1 ---> nLine
+            if i == nLine:
+                # CalculDuNombreDesKmers
+                numbKmers = len(pline) - hope + 1
+                print("Le nombre des kmers:", numbKmers)
+                print("La longueur de la séquence dans la ligne:", i, "est:", len(pline), "nucléotides!")
+                print("La séquence est la suivante:", pline)
 
-    print("Le temps du processus:", processTiming, "secondes,en minutes:", processTiming/60)
-    mem.print_diff()
+                # PourLe S commençant de la position 0 et avec un hope de longeur k, jusquau dernier kmer, Split to Kmers of HopeLength,PisIncrémenter s
+                for s in range(0, len(pline) - hope):
+                    sub = pline[s:s + hope]
+                    if sub in newdict:
+                        newdict[sub] += 1
+                    # Sinon ajouter la première Ocurrence
+                    else:
+                        newdict[sub] = 1
 
+                # Passer à la prochaine ligne de séquence
+                nLine = nLine + 4
+        # Le dictionnaire avec le nombre d'occurences
 
+        # ChronoEnd
+        end = time.time()
+        # Calcul de la durée du Processus
+        processTiming = end - start
+        print(newdict)
+        print("Le temps du processus:", processTiming, "secondes,en minutes:", processTiming / 60)
+        mem.print_diff()
+
+processingfunc("chry5_R1.fastq", 4)
 
 
 
