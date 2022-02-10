44bfbb049bbbf1c1d0b1673bbb2b3bd67e5113ec
Manjoura98 <89964355+Manjoura98@users.noreply.github.com>
2022-02-10 03:24:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,33 +3,27 @@ from pympler import tracker
 import contextlib
 
 fname = input("What file do you want to work with:")
-hope = input("What is your k:")
-
-hope = int(hope)
+hope = int(input("What is your k:"))
 mem = tracker.SummaryTracker()
 # ChronoStart
 start = time.time()
 # OuvertureDuFichier
 with open(fname, "r") as file:
-        # DéclarationDuDictio
+    # DéclarationDuDictio
     newdict = {}
-        # InitialisationDeLaPremièreLigneAtraiter
+    # InitialisationDeLaPremièreLigneAtraiter
     nLine = 1
-        # LaLongeurDuKmer
-
-    hope = int(hope)
-        # IterationSurLesLignesDuFichierPourTraiterLesLignesDesSéquences
+    # IterationSurLesLignesDuFichierPourTraiterLesLignesDesSéquences
     for i, line in enumerate(file):
         pline = line.replace('N', '')
-            # ÇavaCommencerÀLaLigneAvecIndex=1 ---> nLine
+        # CommencerÀLaLigneAvecIndex=1 ---> nLine
         if i == nLine:
-                # CalculDuNombreDesKmers
+            # CalculDuNombreDesKmers
             numbKmers = len(pline) - hope + 1
             print("Le nombre des kmers:", numbKmers)
             print("La longueur de la séquence dans la ligne:", i, "est:", len(pline), "nucléotides!")
             print("La séquence est la suivante:", pline)
-
-                # PourLe S commençant de la position 0 et avec un hope de longeur k, jusquau dernier kmer, Split to Kmers of HopeLength,PisIncrémenter s
+            # PourLe S commençant de la position 0 et avec un hope de longeur k, jusquau dernier kmer, Split to Kmers of HopeLength,PisIncrémenter s
             for s in range(0, len(pline) - hope):
                 sub = pline[s:s + hope]
                 if sub in newdict:
@@ -37,26 +31,21 @@ with open(fname, "r") as file:
                     # Sinon ajouter la première Ocurrence
                 else:
                     newdict[sub] = 1
-
                 # Passer à la prochaine ligne de séquence
             nLine = nLine + 4
-        # Le dictionnaire avec le nombre d'occurences
-
 # ChronoEnd
 end = time.time()
 # Calcul de la durée du Processus
 processTiming = end - start
 saver = input("What is the name of file you want to save the results in:")
-with open(saver,"w+") as rfile:
+with open(saver, "w+") as rfile:
     with contextlib.redirect_stdout(rfile):
         print(newdict)
         print("Le temps du processus:", processTiming, "secondes,en minutes:", processTiming / 60)
         mem.print_diff()
         rfile.close()
 
-
-#Author Mehdi Manjoura AKA MM98
-#Feb 8, 2022
-
+# Author Mehdi Manjoura AKA MM98
+# Feb 8, 2022
 
 
